db179fd879a98e78eb4142373cc393ebd7379a57
Nikita Diulin <nikitadiulin@gmail.com>
2026-09-05 17:03:48 +0700

    Lab1: в main добавлено меню с возможностью выбора задания

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,45 @@
-﻿//Lab1. Вычисление факториала
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа 1. Вариант 5\n");
+            bool exit = false;
+
+            while (!exit)
+            {
+                Menu();
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Factorial();
+                        break;
+                    case "2":
+                        Fibonacci();
+                        break;
+                    case "3":
+                        Function();
+                        break;
+                    case "4":
+                        Taylor();
+                        break;
+                    case "5":
+                        exit = true;
+                        Console.WriteLine("Выход");
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка: введите число от 0 до 20.\n");
+                        break;
+                }
+            }
+        }
+    }
+}
+//Lab1. Вычисление факториала
 Console.Write("Введите n (0..20): ");
 
 if (!int .TryParse(Console.ReadLine(), out int n) || n < 0 || n > 20)
